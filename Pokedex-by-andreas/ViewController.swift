6f786af8161244687bdde49963982c3f14ec8ffd
Andreas Ryde <webdesign@ryde.no>
2016-06-09 12:46:39 +0200

    Initial Commit

---
+++ Pokedex-by-andreas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokedex-by-andreas
+//
+//  Created by Andreas Ryde on 09.06.2016.
+//  Copyright © 2016 Andreas Ryde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
