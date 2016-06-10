87f79a6ba4f5fe47bc07c5fdf19daa9a77c4a8de
Andreas Ryde <webdesign@ryde.no>
2016-06-10 13:01:04 +0200

    Added music and launchscreen image

--- Pokedex-by-andreas/ViewController.swift
+++ Pokedex-by-andreas/ViewController.swift
@@ -36,8 +36,8 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         do {
             musicPlayer = try AVAudioPlayer(contentsOfURL: NSURL(string: path)!)
             musicPlayer.prepareToPlay()
-            //musicPlayer.numberOfLoops = -1
-            //musicPlayer.play()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
         } catch let err as NSError {
             print(err.debugDescription)
         }
